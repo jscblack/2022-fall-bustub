55cb2cd7a78d1047fc105c77c9e79fdf65b28be5
jscblack <jscblack@163.com>
2022-11-01 21:52:26 +0800

    Finish proj1-lru_k_policy

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -14,16 +14,148 @@
 
 namespace bustub {
 
-LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
+LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {
+  frame_arr_.reserve(replacer_size_);
+  for (size_t i = 0; i < num_frames; i++) {
+    frame_arr_.emplace_back(Frame(k_));
+  }
+}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+  std::scoped_lock<std::mutex> lock(latch_);
+  size_t ps = SIZE_MAX;
+  size_t longest_interval = 0;
+  for (size_t i = 0; i < frame_arr_.size(); i++) {
+    if (frame_arr_.at(i).IsInReplacer() && frame_arr_.at(i).GetEvictable()) {
+      if (frame_arr_.at(i).GetLastKthAcc(current_timestamp_) == SIZE_MAX) {
+        // this is not kth
+        if (longest_interval == SIZE_MAX) {
+          // muliple
+          ps = SIZE_MAX;
+          break;
+        }
+        ps = i;
+        longest_interval = frame_arr_.at(i).GetLastKthAcc(current_timestamp_);
+      } else if (frame_arr_.at(i).GetLastKthAcc(current_timestamp_) > longest_interval) {
+        ps = i;
+        longest_interval = frame_arr_.at(i).GetLastKthAcc(current_timestamp_);
+      }
+    }
+  }
+  if (ps != SIZE_MAX) {
+    frame_arr_.at(ps).Evict();
+    *frame_id = static_cast<frame_id_t>(ps);
+    curr_size_--;
+    return true;
+  }
+  // use classic lru
+  longest_interval = 0;
+  for (size_t i = 0; i < frame_arr_.size(); i++) {
+    if (frame_arr_.at(i).IsInReplacer() && frame_arr_.at(i).GetEvictable() &&
+        frame_arr_.at(i).GetLastKthAcc(current_timestamp_) == SIZE_MAX) {
+      if (frame_arr_.at(i).GetLastAcc(current_timestamp_) > longest_interval) {
+        ps = i;
+        longest_interval = frame_arr_.at(i).GetLastAcc(current_timestamp_);
+      }
+    }
+  }
+  if (ps != SIZE_MAX) {
+    frame_arr_.at(ps).Evict();
+    *frame_id = static_cast<frame_id_t>(ps);
+    curr_size_--;
+    return true;
+  }
+  return false;
+}
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id) {}
+void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
+  std::scoped_lock<std::mutex> lock(latch_);
+  // BUSTUB_ASSERT((static_cast<size_t>(frame_id) > replacer_size_), "frame_id overflow");
+  if (static_cast<size_t>(frame_id) >= replacer_size_) {
+    return;
+  }
+  frame_arr_.at(frame_id).Access(current_timestamp_++);
+}
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+  std::scoped_lock<std::mutex> lock(latch_);
+  // BUSTUB_ASSERT((static_cast<size_t>(frame_id) > replacer_size_), "frame_id overflow");
+  if (static_cast<size_t>(frame_id) >= replacer_size_) {
+    return;
+  }
+  if (!frame_arr_.at(frame_id).IsInReplacer()) {
+    return;
+  }
+  if (frame_arr_.at(frame_id).GetEvictable() && !set_evictable) {
+    curr_size_--;
+    frame_arr_.at(frame_id).SetEvictable(set_evictable);
+  } else if (!frame_arr_.at(frame_id).GetEvictable() && set_evictable) {
+    curr_size_++;
+    frame_arr_.at(frame_id).SetEvictable(set_evictable);
+  }
+}
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+  std::scoped_lock<std::mutex> lock(latch_);
+  if (static_cast<size_t>(frame_id) >= replacer_size_) {
+    return;
+  }
+  if (!frame_arr_.at(frame_id).IsInReplacer()) {
+    return;
+  }
+  // BUSTUB_ASSERT((!frame_arr_.at(frame_id).GetEvictable()), "frame is not Evictable");
+  if (!frame_arr_.at(frame_id).GetEvictable()) {
+    return;
+  }
+  curr_size_--;
+  frame_arr_.at(frame_id).Evict();
+}
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+auto LRUKReplacer::Size() -> size_t {
+  std::scoped_lock<std::mutex> lock(latch_);
+  return curr_size_;
+}
+
+//========================//
+LRUKReplacer::Frame::Frame(size_t size) : size_(size) {
+  last_access_timestamp_.resize(size_);
+  in_replacer_ = false;
+  evictable_ = false;
+  access_ptr_ = 0;
+  for (auto &i : last_access_timestamp_) {
+    i = SIZE_MAX;
+  }
+}
+void LRUKReplacer::Frame::Evict() {
+  in_replacer_ = false;
+  evictable_ = false;
+  access_ptr_ = 0;
+  for (auto &i : last_access_timestamp_) {
+    i = SIZE_MAX;
+  }
+}
+
+void LRUKReplacer::Frame::Access(size_t timestamp) {
+  if (!in_replacer_) {
+    in_replacer_ = true;
+  }
+  last_access_timestamp_.at(access_ptr_) = timestamp;
+  access_ptr_ = (access_ptr_ + 1) % size_;
+}
+
+auto LRUKReplacer::Frame::GetLastKthAcc(size_t timestamp) const -> size_t {
+  if (last_access_timestamp_.at(access_ptr_) == SIZE_MAX) {
+    return SIZE_MAX;
+  }
+  return timestamp - last_access_timestamp_.at(access_ptr_);
+}
+auto LRUKReplacer::Frame::GetLastAcc(size_t timestamp) const -> size_t {
+  size_t target_ptr = access_ptr_;
+  while (last_access_timestamp_.at(target_ptr) == SIZE_MAX) {
+    target_ptr++;
+    target_ptr %= size_;
+  }
+  return timestamp - last_access_timestamp_.at(target_ptr);
+}
 
 }  // namespace bustub
--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -27,12 +27,12 @@ namespace bustub {
  * LRUKReplacer implements the LRU-k replacement policy.
  *
  * The LRU-k algorithm evicts a frame whose backward k-distance is maximum
- * of all frames. Backward k-distance is computed as the difference in time between
- * current timestamp and the timestamp of kth previous access.
+ * of all frames. Backward k-distance is computed as the difference in time
+ * between current timestamp and the timestamp of kth previous access.
  *
  * A frame with less than k historical references is given
- * +inf as its backward k-distance. When multiple frames have +inf backward k-distance,
- * classical LRU algorithm is used to choose victim.
+ * +inf as its backward k-distance. When multiple frames have +inf backward
+ * k-distance, classical LRU algorithm is used to choose victim.
  */
 class LRUKReplacer {
  public:
@@ -41,7 +41,8 @@ class LRUKReplacer {
    * TODO(P1): Add implementation
    *
    * @brief a new LRUKReplacer.
-   * @param num_frames the maximum number of frames the LRUReplacer will be required to store
+   * @param num_frames the maximum number of frames the LRUReplacer will be
+   * required to store
    */
   explicit LRUKReplacer(size_t num_frames, size_t k);
 
@@ -57,29 +58,33 @@ class LRUKReplacer {
   /**
    * TODO(P1): Add implementation
    *
-   * @brief Find the frame with largest backward k-distance and evict that frame. Only frames
-   * that are marked as 'evictable' are candidates for eviction.
+   * @brief Find the frame with largest backward k-distance and evict that
+   * frame. Only frames that are marked as 'evictable' are candidates for
+   * eviction.
    *
-   * A frame with less than k historical references is given +inf as its backward k-distance.
-   * If multiple frames have inf backward k-distance, then evict the frame with the earliest
-   * timestamp overall.
+   * A frame with less than k historical references is given +inf as its
+   * backward k-distance. If multiple frames have inf backward k-distance, then
+   * evict the frame with the earliest timestamp overall.
    *
-   * Successful eviction of a frame should decrement the size of replacer and remove the frame's
-   * access history.
+   * Successful eviction of a frame should decrement the size of replacer and
+   * remove the frame's access history.
    *
    * @param[out] frame_id id of frame that is evicted.
-   * @return true if a frame is evicted successfully, false if no frames can be evicted.
+   * @return true if a frame is evicted successfully, false if no frames can be
+   * evicted.
    */
   auto Evict(frame_id_t *frame_id) -> bool;
 
   /**
    * TODO(P1): Add implementation
    *
-   * @brief Record the event that the given frame id is accessed at current timestamp.
-   * Create a new entry for access history if frame id has not been seen before.
+   * @brief Record the event that the given frame id is accessed at current
+   * timestamp. Create a new entry for access history if frame id has not been
+   * seen before.
    *
-   * If frame id is invalid (ie. larger than replacer_size_), throw an exception. You can
-   * also use BUSTUB_ASSERT to abort the process if frame id is invalid.
+   * If frame id is invalid (ie. larger than replacer_size_), throw an
+   * exception. You can also use BUSTUB_ASSERT to abort the process if frame id
+   * is invalid.
    *
    * @param frame_id id of frame that received a new access.
    */
@@ -88,16 +93,18 @@ class LRUKReplacer {
   /**
    * TODO(P1): Add implementation
    *
-   * @brief Toggle whether a frame is evictable or non-evictable. This function also
-   * controls replacer's size. Note that size is equal to number of evictable entries.
+   * @brief Toggle whether a frame is evictable or non-evictable. This function
+   * also controls replacer's size. Note that size is equal to number of
+   * evictable entries.
    *
-   * If a frame was previously evictable and is to be set to non-evictable, then size should
-   * decrement. If a frame was previously non-evictable and is to be set to evictable,
-   * then size should increment.
+   * If a frame was previously evictable and is to be set to non-evictable, then
+   * size should decrement. If a frame was previously non-evictable and is to be
+   * set to evictable, then size should increment.
    *
    * If frame id is invalid, throw an exception or abort the process.
    *
-   * For other scenarios, this function should terminate without modifying anything.
+   * For other scenarios, this function should terminate without modifying
+   * anything.
    *
    * @param frame_id id of frame whose 'evictable' status will be modified
    * @param set_evictable whether the given frame is evictable or not
@@ -107,15 +114,16 @@ class LRUKReplacer {
   /**
    * TODO(P1): Add implementation
    *
-   * @brief Remove an evictable frame from replacer, along with its access history.
-   * This function should also decrement replacer's size if removal is successful.
+   * @brief Remove an evictable frame from replacer, along with its access
+   * history. This function should also decrement replacer's size if removal is
+   * successful.
    *
-   * Note that this is different from evicting a frame, which always remove the frame
-   * with largest backward k-distance. This function removes specified frame id,
-   * no matter what its backward k-distance is.
+   * Note that this is different from evicting a frame, which always remove the
+   * frame with largest backward k-distance. This function removes specified
+   * frame id, no matter what its backward k-distance is.
    *
-   * If Remove is called on a non-evictable frame, throw an exception or abort the
-   * process.
+   * If Remove is called on a non-evictable frame, throw an exception or abort
+   * the process.
    *
    * If specified frame is not found, directly return from this function.
    *
@@ -132,14 +140,33 @@ class LRUKReplacer {
    */
   auto Size() -> size_t;
 
+  class Frame {
+   public:
+    explicit Frame(size_t size);
+    void Evict();
+    void Access(size_t timestamp);
+    auto GetLastKthAcc(size_t timestamp) const -> size_t;
+    auto GetLastAcc(size_t timestamp) const -> size_t;
+    inline auto GetEvictable() const -> bool { return evictable_; }
+    inline void SetEvictable(bool evictable) { evictable_ = evictable; }
+    inline auto IsInReplacer() const -> bool { return in_replacer_; }
+
+   private:
+    bool in_replacer_{false};
+    bool evictable_{false};
+    size_t size_;
+    size_t access_ptr_{0};
+    std::vector<size_t> last_access_timestamp_;
+  };
+
  private:
-  // TODO(student): implement me! You can replace these member variables as you like.
-  // Remove maybe_unused if you start using them.
-  [[maybe_unused]] size_t current_timestamp_{0};
-  [[maybe_unused]] size_t curr_size_{0};
-  [[maybe_unused]] size_t replacer_size_;
-  [[maybe_unused]] size_t k_;
+  // TODO(student): implement me! You can replace these member variables as you
+  // like. Remove maybe_unused if you start using them.
+  size_t current_timestamp_{0};
+  std::vector<Frame> frame_arr_;
+  size_t curr_size_{0};
+  size_t replacer_size_;
+  size_t k_;
   std::mutex latch_;
 };
-
 }  // namespace bustub
